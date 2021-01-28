bc1b0e9301bbe8cd47747573a20507aae1b72d47
Konrad <konrad.gumienny@gmail.com>
2021-01-28 19:15:00 +0100

    remove cached repo

---
+++ PantryBackend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = product::all()->toArray();
+        return $products;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new product;
+        $product->product_name = $request->product_name;
+        $product->quantity = $request->quantity;
+        $product->weight = $request->weight;
+        $product->category = $request->category;
+        $product->exp_date = $request->exp_date;
+        $product->place = $request->place;
+        $product->comments = $request->comments;
+        $product->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, product $product)
+    {
+        $product->product_name = $request->product_name;
+        $product->quantity = $request->quantity;
+        $product->weight = $request->weight;
+        $product->category = $request->category;
+        $product->exp_date = $request->exp_date;
+        $product->place = $request->place;
+        $product->comments = $request->comments;
+        $product->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(product $product)
+    {
+        $product->delete();
+    }
+}
---
+++ PantryBackend/database/migrations/2021_01_24_132339_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->char("product_name", 255);
+            $table->integer("quantity");
+            $table->char("weight", 255);
+            $table->char("category", 255);
+            $table->timestamp('exp_date', $precision = 0);
+            $table->char("place", 255);
+            $table->string('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
